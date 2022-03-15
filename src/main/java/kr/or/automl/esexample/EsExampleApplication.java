831fd75c64de522802e2ed6d31486fac95ad3ead
pangnem <sprnd645@gmail.com>
2022-03-15 17:47:37 +0900

    refactor: 테스트를 위해 검색엔진 저장소 컴포넌트 스캔 위치 변경
    - EsExampleApplication -> JpaConfig

--- src/main/java/kr/or/automl/esexample/EsExampleApplication.java
+++ src/main/java/kr/or/automl/esexample/EsExampleApplication.java
@@ -1,20 +1,8 @@
 package kr.or.automl.esexample;
 
-import kr.or.automl.esexample.domain.file.search.FileSearchRepository;
-import kr.or.automl.esexample.domain.user.search.UserSearchRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = {
-                UserSearchRepository.class,
-                FileSearchRepository.class
-        }
-))
 @SpringBootApplication
 public class EsExampleApplication {
 
--- src/main/java/kr/or/automl/esexample/config/JpaConfig.java
+++ src/main/java/kr/or/automl/esexample/config/JpaConfig.java
@@ -1,9 +1,28 @@
 package kr.or.automl.esexample.config;
 
+import kr.or.automl.esexample.domain.file.FileRepository;
+import kr.or.automl.esexample.domain.file.search.FileSearchRepository;
+import kr.or.automl.esexample.domain.user.UserRepository;
+import kr.or.automl.esexample.domain.user.search.UserSearchRepository;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackageClasses = {
+        UserRepository.class,
+        FileRepository.class
+},
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {
+                        UserSearchRepository.class,
+                        FileSearchRepository.class
+                }
+        )
+)
 @Configuration
 public class JpaConfig {
 }
