6dea5dd7e7d520eaa5be6b582d8dc3179d76cb9d
pangnem <sprnd645@gmail.com>
2022-03-22 15:39:14 +0900

    fix: 베이스 Jpa 클래스 변경

--- src/main/java/kr/or/automl/esexample/config/JpaConfig.java
+++ src/main/java/kr/or/automl/esexample/config/JpaConfig.java
@@ -1,9 +1,9 @@
 package kr.or.automl.esexample.config;
 
-import kr.or.automl.esexample.domain.file.FileRepository;
 import kr.or.automl.esexample.domain.file.search.FileSearchRepository;
 import kr.or.automl.esexample.domain.user.UserRepository;
 import kr.or.automl.esexample.domain.user.search.UserSearchRepository;
+import kr.or.automl.esexample.infra.JpaFileRepository;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackageClasses = {
         UserRepository.class,
-        FileRepository.class
+        JpaFileRepository.class
 },
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.ASSIGNABLE_TYPE,
